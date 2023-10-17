54d635719ef2050edb87e72b7b2cd4e87692e822
rebcop <76892971+rebcop@users.noreply.github.com>
2023-10-17 01:18:58 -0400

    completed filpflop180

--- js/site.js
+++ js/site.js
@@ -1,12 +1,51 @@
-function displayMessage() {
-    // get the text out of our <input> and assign it to a variable
-    let msg = document.getElementById('message').value;
+// get the user's message to reverse
+function getValues() {
 
-    Swal.fire(
-        {
+    // get the text input from the page
+    let message = document.getElementById('userInput').value;
+
+    // validate the input: make sure the input is not empty,could do at least two char
+    if (message.length == 0 ) {
+
+        swal.fire({
+            icon: 'error',
             backdrop: false,
-            title: 'APP NAME',
-            text: msg, // <-- replace this text with that variable
-        }
-    );
+            text: 'Please enter a string to reverse'
+        });
+
+
+    } else {
+
+        // pass the input to reverseMessage function and assign its return value to a variable
+        let revMsg = reverseMessage(message);
+
+        // give the reversed message to displayMessage to show on the page
+        displayMessage(revMsg);
+
+    }
+
+}
+
+// reverse the string
+
+// IMPORTANT TRICK: DID YOU KNOW STRING IS AN ARRAY OF CHARACTERS
+function reverseMessage(input) { // input = 'hello'
+
+    output = '';
+
+    for (let i = input.length - 1; i >= 0; i--) {
+
+        output += input[i];
+    }
+
+    return output;
+}
+
+// display the reversed message
+function displayMessage(message) {
+
+    document.getElementById('msg').textContent = `Your message reversed is: ${message}`;
+
+    document.getElementById('alert').classList.remove('invisible');
+
 }
